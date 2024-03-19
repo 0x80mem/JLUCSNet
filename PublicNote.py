334c8a951b9f2010f3ed22698802d383a196efb9
ChengYu <143814637+114514Cheng@users.noreply.github.com>
2024-03-19 15:24:11 +0800

    Update PublicNote.py

--- PublicNote.py
+++ PublicNote.py
@@ -5,101 +5,104 @@ import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-url1 = 'https://ccst.jlu.edu.cn/xwzx/xytz.htm'
-url2 = 'https://ccst.jlu.edu.cn/xwzx/xyxw.htm'
-url3 = 'https://ccst.jlu.edu.cn/xwzx/xstd.htm'
-url4 = 'https://ccst.jlu.edu.cn/xwzx/zsjy.htm'
-url5 = 'https://ccst.jlu.edu.cn/xwzx/gsl.htm'
 
-urls = [url1,url2,url3,url4,url5]
+def work():
+    url1 = 'https://ccst.jlu.edu.cn/xwzx/xytz.htm'
+    url2 = 'https://ccst.jlu.edu.cn/xwzx/xyxw.htm'
+    url3 = 'https://ccst.jlu.edu.cn/xwzx/xstd.htm'
+    url4 = 'https://ccst.jlu.edu.cn/xwzx/zsjy.htm'
+    url5 = 'https://ccst.jlu.edu.cn/xwzx/gsl.htm'
 
-prelog = 'https://ccst.jlu.edu.cn'
-patternDrop = re.compile(r'<p[^>]*>|<\/p>|<br>|<br[^>]*>|&nbsp;|<span[^>]*>|'
-                         r'<\/span>|<li[^>]*>|</li>|<ol[^>]*>|<\/ol>|'
-                         r'<\/strong>|<ul[^>]*>|<\/ul>|<h1[^>]*>|</h1>|<td[^>]*>|</td>|'
-                         r'<div[^>]*>|</div>|</form>|<h2[^>]*>|</h2>|<tbody>|<table>|<a[^>]*>|</a>|</tbody>|</table>|\n|\r|\u3000|\xa0')
+    urls = [url1, url2, url3, url4, url5]
 
-patternStrong = re.compile(r'<strong[^>]*>',re.S)
+    prelog = 'https://ccst.jlu.edu.cn'
+    patternDrop = re.compile(r'<p[^>]*>|<\/p>|<br>|<br[^>]*>|&nbsp;|<span[^>]*>|'
+                             r'<\/span>|<li[^>]*>|</li>|<ol[^>]*>|<\/ol>|'
+                             r'<\/strong>|<ul[^>]*>|<\/ul>|<h1[^>]*>|</h1>|<td[^>]*>|</td>|'
+                             r'<div[^>]*>|</div>|</form>|<h2[^>]*>|</h2>|<tbody>|<table>|<a[^>]*>|</a>|</tbody>|</table>|\n|\r|\u3000|\xa0')
 
-dics = []
-titlexpath = '/html/head/title[1]'
+    patternStrong = re.compile(r'<strong[^>]*>', re.S)
 
-t = 0
-while t != 5:
-    content = []
-    dict = {
-        'url': [],
-        'title': [],
-        'content': [],
-        'date': []
-    }
-    pagexpath = '/html/body/div[3]/div[2]/div[2]/div[1]/span[1]'
-    resp = requests.get(urls[t])
-    html = etree.HTML(resp.content)
-    divs = html.xpath(pagexpath)
-    for div in divs:
-        rows = div.text[1:-1]
+    dics = []
+    titlexpath = '/html/head/title[1]'
 
-    pages = math.ceil(int(rows)/10)
-    curpages = pages
+    t = 0
+    while t != 5:
+        content = []
+        dict = {
+            'url': [],
+            'title': [],
+            'content': [],
+            'date': []
+        }
+        pagexpath = '/html/body/div[3]/div[2]/div[2]/div[1]/span[1]'
+        resp = requests.get(urls[t])
+        html = etree.HTML(resp.content)
+        divs = html.xpath(pagexpath)
+        for div in divs:
+            rows = div.text[1:-1]
 
-    # 先获取页面的动态html，将其转化为静态代码
-    while curpages != 0:
-        i = 1
-        browser = webdriver.Edge()
-        if curpages == pages:  # 说明当前在众多页中的第一页，则url不需要改变
-            browser.get(urls[t])
-            curpages -= 1
-        else:  # url需要改变
-            url = urls[t][:-4] + '/' + str(curpages) + urls[t][-4:]
-            curpages -= 1
-            browser.get(url)
+        pages = math.ceil(int(rows) / 10)
+        curpages = pages
 
-        htmlStr = browser.page_source
-        # print(htmlStr)  # 将第t页的动态码转换为静态字串
-        browser.close()
-        html = etree.HTML(htmlStr)  # 把静态字串转换为HTML
+        # 先获取页面的动态html，将其转化为静态代码
+        while curpages != 0:
+            i = 1
+            browser = webdriver.Edge()
+            if curpages == pages:  # 说明当前在众多页中的第一页，则url不需要改变
+                browser.get(urls[t])
+                curpages -= 1
+            else:  # url需要改变
+                url = urls[t][:-4] + '/' + str(curpages) + urls[t][-4:]
+                curpages -= 1
+                browser.get(url)
 
-        # 下面开始获取li
-        i = 1
-        while i != 11:
-            rowxpath = f'/html/body/div[3]/div[2]/div[2]/ul[1]/li[{i}]/a[1]'  # 获取连接用的xpath
-            divs = html.xpath(rowxpath)  # 对HTML进行xpath解析（这一页包含很多li）
-            for div in divs:
-                content = []
-                newUrl = prelog + div.get('href')[2:]
-                # print(newUrl)
-                # 下面对这个newUrl进行访问，并获取文章内容
-                resp2 = requests.get(newUrl)
-                resp2_headers = resp2.headers
-                lastmodify = resp2_headers.get('Last-Modified')
-                html2 = etree.HTML(resp2.content)
-                titles = html2.xpath(titlexpath)
-                for e in titles:
-                    title = e.text  # 获取标题
+            htmlStr = browser.page_source
+            # print(htmlStr)  # 将第t页的动态码转换为静态字串
+            browser.close()
+            html = etree.HTML(htmlStr)  # 把静态字串转换为HTML
 
-                soup = BeautifulSoup(resp2.content, 'html.parser')
+            # 下面开始获取li
+            i = 1
+            while i != 11:
+                rowxpath = f'/html/body/div[3]/div[2]/div[2]/ul[1]/li[{i}]/a[1]'  # 获取连接用的xpath
+                divs = html.xpath(rowxpath)  # 对HTML进行xpath解析（这一页包含很多li）
+                for div in divs:
+                    content = []
+                    newUrl = prelog + div.get('href')[2:]
+                    # print(newUrl)
+                    # 下面对这个newUrl进行访问，并获取文章内容
+                    resp2 = requests.get(newUrl)
+                    resp2_headers = resp2.headers
+                    lastmodify = resp2_headers.get('Last-Modified')
+                    html2 = etree.HTML(resp2.content)
+                    titles = html2.xpath(titlexpath)
+                    for e in titles:
+                        title = e.text  # 获取标题
 
-                # 找到id为vsb_content的div标签
-                vsb_content_div = soup.find('div', id=['vsb_content','vsb_content_100'])
+                    soup = BeautifulSoup(resp2.content, 'html.parser')
 
-                # 获取该div内部的所有内容（包括标签）的字符串表示
-                inner_content = str(vsb_content_div)
-                inner_content = patternDrop.sub('', inner_content)
-                subStrings = inner_content.split('</p>')
+                    # 找到id为vsb_content的div标签
+                    vsb_content_div = soup.find('div', id=['vsb_content', 'vsb_content_100'])
 
-                for it in subStrings:
-                    patternDrop.sub('', it)
-                    patternStrong.sub('', it)
-                    if it != '':
-                        content.append(it)
+                    # 获取该div内部的所有内容（包括标签）的字符串表示
+                    inner_content = str(vsb_content_div)
+                    inner_content = patternDrop.sub('', inner_content)
+                    subStrings = inner_content.split('</p>')
 
-                dict['url'] = newUrl
-                dict['content'] = content
-                dict['title'] = title
-                dict['date'] = lastmodify
-                dics.append(dict)
-                print(dics)
-            i += 1
+                    for it in subStrings:
+                        patternDrop.sub('', it)
+                        patternStrong.sub('', it)
+                        if it != '':
+                            content.append(it)
 
-    t += 1
+                    dict['url'] = newUrl
+                    dict['content'] = content
+                    dict['title'] = title
+                    dict['date'] = lastmodify
+                    dics.append(dict)
+                    print(dics)
+                i += 1
+
+        t += 1
+    return dics
